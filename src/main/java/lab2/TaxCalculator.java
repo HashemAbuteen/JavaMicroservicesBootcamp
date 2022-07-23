b41aff5b4a397bb17eb877c3770ef9bc81ffb79a
Hashem <hashemabualteen@gmail.com>
2022-07-23 09:41:11 +0300

    implemented tax calculator

--- src/main/java/lab2/TaxCalculator.java
+++ src/main/java/lab2/TaxCalculator.java
@@ -3,15 +3,32 @@ package lab2;
 public class TaxCalculator {
     public static void main(String[] args) {
         double salary = Math.random()*50000 + 5000;
-        if(salary < 23000){
-            System.out.println("before tax : " + salary);
-            System.out.println(salary + " + 10% = ");
-            System.out.println("after tax : " + salary + 0.1*salary);
-        }
-        else if(salary < 23000){
-            System.out.println("before tax : " + salary);
-            System.out.println(salary + " + 10% = ");
-            System.out.println("after tax : " + salary + 0.1*salary);
+        double tax;
+        if(0<salary && salary<23000){
+
+            tax = salary *0.1;
+            System.out.println("before Tax : "+salary);
+            System.out.println("Tax : "+tax);
+            System.out.println("After tax "+(salary-tax));
+
+
+        } else if (23000<salary && salary<50000) {
+            tax = salary *0.2;
+            System.out.println("before Tax : "+salary);
+            System.out.println("Tax : "+tax);
+            System.out.println("After tax "+(salary-tax));
+
+        }else if (50000<salary && salary<100000) {
+
+            tax = salary *0.3;
+            System.out.println("before Tax : "+salary);
+            System.out.println("Tax : "+tax);
+            System.out.println("After tax "+(salary-tax));
+        }else if (100000<salary) {
+            tax = salary *0.4;
+            System.out.println("before Tax : "+salary);
+            System.out.println("Tax : "+tax);
+            System.out.println("After tax "+(salary-tax));
         }
     }
 }
